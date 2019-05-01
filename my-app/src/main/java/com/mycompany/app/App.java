5716674a6e7735a84c4db8919c1e1ec958d69bc7
Jake Angerman <jake@redislabs.com>
2019-05-01 10:44:24 -0400

    pretty print results, fix log4j warning via pom.xml

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -7,26 +7,58 @@ import io.redisearch.Query;
 import io.redisearch.Schema;
 
 /**
- * Hello world!
+ * query RediSearch
  *
  */
 public class App 
 {
+    protected static Client client = new Client("myIndex", "localhost", 6379);
+
     public static void main( String[] argv )
     {
-        System.out.println( "Hello World!" );
+	final int BATCH_SIZE = 10; // batch size for query results
 
-	Client client = new Client("myIndex", "localhost", 6379);
+	if (argv[0].equals("schema")) {
+		createSchema();
+		System.exit(0);
+	}
 
-	// Creating a complex query
-	Query q = new Query(argv[0]);
+	// create a query from a string
+	Query query = new Query(argv[0]);
 
-	// run query
-	long start = System.currentTimeMillis();
-	SearchResult results = client.search(q);
-	long totalMilliseconds = System.currentTimeMillis() - start;
+	// execute query
+	SearchResult results = client.search(query);  // first batch
+	long totalResults = results.totalResults;
+	System.out.printf("Number of documents: %d\n", totalResults);
 
-	System.out.printf("Number of documents: %d, Query time: %d ms\n", results.totalResults, totalMilliseconds);
+	// print results
+	int i = 0;
+	while (i < totalResults) {
+		for (Document doc : results.docs) {
+			System.out.printf("%s\n", doc.toString());
+			i++;
+		}
+		if (i < totalResults) {
+			// get the next batch
+			results = client.search(query.limit(i, BATCH_SIZE));
+		}
+	}
+
 	System.exit(0);
+    }
+
+    protected static void createSchema( ) {
+
+	System.out.println("Creating new schema.");
+	// public TextField(String name, double weight, boolean sortable, boolean nostem)
+	Schema schema = new Schema()
+            .addField(new Schema.TextField("group", 1.0, false, true))
+            .addField(new Schema.TextField("code", 1.0, false, true))
+            .addTagField("type")
+            .addField(new Schema.TextField("qty", 1.0, false, true))
+            .addField(new Schema.TextField("days", 1.0, false, true))
+            .addField(new Schema.TextField("search", 1.0, false, true));
+
+	client.createIndex(schema, Client.IndexOptions.Default());
     }
 }
